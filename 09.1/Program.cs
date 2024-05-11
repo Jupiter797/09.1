55f4b1e7e59a11c869b13ff7aaa9ffe1c5bf2f38
Serhii Pypa <jupiters797@gmail.com>
2024-05-11 11:27:30 +0300

    Добавьте файлы проекта.

---
+++ 09.1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._1
+{
+    class Property
+    {
+        private char charField;
+        private int intField;
+
+        public string textProperty
+        {
+            get
+            {
+                return $"Символ : {charField} | Значення : {intField}";
+            }
+        }
+
+        public Property(char charField, int intField)
+        {
+            this.charField = charField;
+            this.intField = intField;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Property property = new Property('A', 10);
+
+            //string fieldsText = property.FieldsText;
+            Console.WriteLine(property.textProperty);
+
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 09.2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._2
+{
+    public class Indexer
+    {
+        private int[] array;
+
+        public Indexer(int size)
+        {
+            array = new int[size];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                if (value % 2 != 0 && value != 0)
+                {
+                    array[index] = value;
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+
+            Console.Write("Введіть розмір масиву : ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Indexer myArray = new Indexer(size);
+
+            Random rand = new Random();
+            int randomNumber;
+            myArray[0] = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                do
+                {
+                    randomNumber = rand.Next(1, 10);
+                } while (randomNumber % 2 == 0 || randomNumber == 0);
+
+                myArray[i] = randomNumber;
+            }
+
+            for (int j = 0; j < size; j++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write($"| {myArray[j]} ");
+            }
+            Console.WriteLine("|");
+            Console.ResetColor();
+
+            Console.CursorVisible = false;
+            Console.ReadKey();
+        }
+    }
+}
